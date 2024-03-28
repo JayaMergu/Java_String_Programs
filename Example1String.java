5de5c1fc43c7690c1cfd6efaae2c16eef4de2a5a
Jaya Mergu <113820845+JayaMergu@users.noreply.github.com>
2024-03-28 22:49:52 +0530

    Add files via upload

---
+++ Example1String.java
@@ -0,0 +1,25 @@
+package com.stringexample;
+
+public class Example1String {
+
+	public static void main(String[] args) {
+		char ch[]= {'D','V','I','Y','A'};
+		String s=new String(ch);
+		System.out.println(ch);
+		String s2="Deepak";//by using literal
+		//String s3=new String("vishal");//by using new keyword
+		String s4="Deepak jjjj";
+		System.out.println(s4);
+		//s4="Deepak Kumar";
+		//System.out.println(s2.hashCode());
+		//System.out.println(s2.hashCode());
+		String s1=new String("Rohan");
+		System.out.println(s1);
+		//s1=s1.concat("kapoor");
+		s2=s1.concat("kapoor");
+		//System.out.println(s1);
+		System.out.println(s2);
+		
+}
+}
+	
